f4e6bf91a76c38e8d5ebc2d8b846b34b4585a05e
Nathaniel J. Smith <njs@pobox.com>
2022-11-05 01:57:10 -0700

    hack

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![windows_subsystem = "console"]
+
+use core::panic::PanicInfo;
+use windows_sys::Win32::System::Threading::ExitProcess;
+
+#[panic_handler]
+pub extern fn panic(_info: &PanicInfo) -> ! { loop {} }
+
+#[no_mangle]
+pub extern fn mainCRTStartup() -> ! {
+    unsafe { ExitProcess(3); }
 }
